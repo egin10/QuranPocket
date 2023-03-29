72bfdaed0efe6612b478dfb89f9e6ff5135bd53c
egin10 <egin066@gmail.com>
2023-03-29 09:34:26 +0700

    Initial Commit

---
+++ QuranPocket/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  QuranPocket
+//
+//  Created by Ginanjar Setia Budi on 29/03/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ QuranPocket/QuranPocketApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuranPocketApp.swift
+//  QuranPocket
+//
+//  Created by Ginanjar Setia Budi on 29/03/23.
+//
+
+import SwiftUI
+
+@main
+struct QuranPocketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
